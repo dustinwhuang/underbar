7d1cf75bcb8dbfde5c4403eef2686daed678b40e
Dustin Huang <mail@dustinhuang.com>
2017-09-11 17:34:19 -0700

    Add delay implementation

--- src/underbar.js
+++ src/underbar.js
@@ -318,6 +318,11 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = Array.prototype.slice.call(arguments, 2);
+
+    setTimeout(function () {
+      return func.apply(this, args);
+    }, wait);
   };
 
 
